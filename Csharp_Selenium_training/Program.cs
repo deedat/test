064f78143d8525ea297a00d6c7582db7386dcd30
Mohamed Benighil <mohamed.benighil@etu.u-pec.fr>
2019-02-27 16:41:41 +0100

    4.Writing Test method using NUnit in Selenium C#  (do not fotget to install "NUnit 3 Test Adapter" and add referrence "NUnit")

--- Csharp_Selenium_training/Program.cs
+++ Csharp_Selenium_training/Program.cs
@@ -1,5 +1,7 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +12,54 @@ namespace Csharp_Selenium_training
 {
     class Program
     {
+        // create aour referrence to our browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
             {
-                // create aour referrence to our browser
-                IWebDriver driver = new ChromeDriver();
 
-                // navigate to google chrome
-                driver.Navigate().GoToUrl("https://www.google.com/");
-
-                //find the element
-                IWebElement element = driver.FindElement(By.Name("q"));
-
-                // perform Ops
-                element.SendKeys("salam");
-
-                //close the brower 
-                driver.Close();
             }
         }
+
+
+
+        [SetUp] // for calling the methode Initialize() immediately before each test
+        public void Initialize()
+        {
+            // navigate to google chrome
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Console.WriteLine("the url is openned");
+        }
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            // perform Ops
+            element.SendKeys("salam");
+            Console.WriteLine("execute peace");
+        }
+
+        [Test]// Second test ()
+        public void SecondTest()
+        {
+            Console.WriteLine("the second test");
+        }
+
+
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //close the brower 
+            driver.Close();
+            Console.WriteLine("the brower is closed!");
+        }
+
     }
 }

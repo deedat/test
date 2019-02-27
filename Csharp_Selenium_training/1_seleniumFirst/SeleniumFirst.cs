0d19c3fd885245137bd2a6994cb5307b438c8d5c
Mohamed Benighil <mohamed.benighil@etu.u-pec.fr>
2019-02-27 14:50:07 +0100

    Add project files.

---
+++ Csharp_Selenium_training/1_seleniumFirst/SeleniumFirst.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Csharp_Selenium_training._1_seleniumFirst
+{
+    class SeleniumFirst
+    {
+        // create aour referrence to our browser
+       // IWebDriver driver = new ChromeDriver();
+        
+
+
+
+
+        // navigate to google chrome
+        // driver.Navigate().GoToUrl("https://www.google.com/");
+
+        //find the element
+        //IWebElement element = driver.FindElement(By.Name("q"));
+
+        // perform Ops
+        //element.SendKeys("salam");
+
+        //close the brower 
+        //  driver.Close();
+    }
+}
---
+++ Csharp_Selenium_training/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Selenium_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // create aour referrence to our browser
+                IWebDriver driver = new ChromeDriver();
+
+                // navigate to google chrome
+                driver.Navigate().GoToUrl("https://www.google.com/");
+
+                //find the element
+                IWebElement element = driver.FindElement(By.Name("q"));
+
+                // perform Ops
+                element.SendKeys("salam");
+
+                //close the brower 
+                driver.Close();
+            }
+        }
+    }
+}
